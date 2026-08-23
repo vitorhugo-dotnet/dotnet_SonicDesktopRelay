30606e84e6069a4be468cf5e0b8a38b13a22937c
Vitor Hugo <vitorhugoalvesferreira@gmail.com>
2026-08-23 06:36:00 -0300

    chore: solution skeleton for SonicDesktopRelay

---
+++ tests/SonicDesktopRelay.Core.Tests/SolutionSmokeTests.cs
@@ -0,0 +1,12 @@
+using Xunit;
+
+namespace SonicDesktopRelay.Core.Tests;
+
+public sealed class SolutionSmokeTests
+{
+    [Fact]
+    public void The_test_harness_runs()
+    {
+        Assert.True(true);
+    }
+}
